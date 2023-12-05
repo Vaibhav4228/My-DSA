1d3d4bc41be74d929700cc372efbb8f8730cc9db
Vaibhav422 <92776886+Vaibhav992@users.noreply.github.com>
2023-12-05 22:04:12 +0530

    bit mnipulation

---
+++ Bit manipulation/cs for all/Bitoperations.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;  // Takes two integer inputs
+  // cout << (a & b) << endl;  // Performs bitwise AND operation and prints the result
+
+
+
+  //Or operator
+  /* 1 1 - 1
+     1 0 - 1
+     0 1 - 1
+     1 1 - 1
+     0 0 - 0
+   */
+  // cout<< (a |  b) << endl;
+  /* XOR
+     1 1 - 0
+     1 0 - 1
+     0 1 - 1
+     1 1 - 1
+     0 0 - 0
+     AGAR 2 ELEMINT SAME HAI TOH 0
+  */
+//  cout<< (a ^ b) << endl;
+//  (~) operator  ye jo hai only 1 operator m legaga if no. 1 is 1 then it is called a -ve number 
+
+  return 0;
+}
