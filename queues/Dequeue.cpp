e90687e9be6e56e39dddecb083d46f9a38e4ca66
Vaibhav422 <92776886+Vaibhav992@users.noreply.github.com>
2023-12-11 23:02:08 +0530

    Dequeue

--- queues/Dequeue.cpp
+++ queues/Dequeue.cpp
@@ -1,2 +1,100 @@
-#include <bits/c++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+class Cqueue{
+  public: 
+   int* arr;
+   int size;
+   int front;
+   int rear;
+   Cqueue(int size){
+    arr = new int[size];
+    this->size = size;
+    front = -1;
+    rear = -1;
+
+   }
+
+   void push(int val){
+    //overflow overflow
+    //empty
+    //circular
+    //normal form
+    if(front == 0 && rear == size-1){
+      cout<<"overflow"<<endl;
+
+    }
+    else if(front == -1 && rear == -1){
+      front++;
+      rear++;
+      arr[front] = val;
+    }
+    else if (rear == size-1 && front !=0){
+      rear = 0;
+      arr[rear] = val;
+    }
+    else {
+      rear++;
+      arr[rear]= val;
+    }
+
+    //empty
+    //circular
+    //normal form
+    
+   }
+   void pop (){
+     //underflow
+     //empty
+     //circular
+     //normal form
+     if(front == -1 && rear == -1){
+      cout<<"underflow"<<endl;
+     }
+     else if (front == rear){
+      arr[front] = -1;
+      front++;
+     }
+     else if(front == size-1){
+      arr[front] = -1;
+      front = 0;
+     }
+     else {
+      arr[front] = -1;
+      front++;
+     }
+   }
+   void print(){
+    cout<<"front: "<<front<<"rear: "<< rear<<endl;
+    cout<<"printing queue"<<endl;
+    for (int i=0; i<size; i++){
+      cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+   }
+};
+
+int main(){
+  Cqueue q(5);
+  q.print();
+
+  q.push(10);
+  q.print();
+
+  q.push(20);
+  q.print();
+
+  q.push(30);
+  q.print();
+
+  q.push(40);
+  q.print();
+
+  q.push(50);
+  q.print();
+
+  q.push(60);
+  q.print();
+
+  return 0;
+}
